a2e395ea7a0fb7d43adc4a1152ae404312184083
ASteinsdoerfer <andreas.steinsdoerfer@gmx.de>
2026-08-16 10:07:25 +0200

    Add error percentage

--- MMM-Gamma601.js
+++ MMM-Gamma601.js
@@ -66,13 +66,12 @@ Module.register("MMM-Gamma601", {
         wrapper.innerHTML = `
             
 			Status ${this.dataMiner.online ? "<i class='fa-solid fa-wifi'></i>" :"<i class='fa-solid fa-bell'></i>"}   <i class="fa-solid fa-trophy"></i> already mined: ${this.dataMiner.Jackpot}<br>
-			<i class="fa-brands fa-btc"></i> Hashrate ${(this.dataMiner.hashrate /1000).toFixed(2)} Th/s<br>
+			<i class="fa-brands fa-btc"></i> Hashrate ${(this.dataMiner.hashrate /1000).toFixed(2)} Th/s   <i class="fa-solid fa-bug"></i> ${this.dataMiner.errorPercentage.toFixed(2)} % <br>
 			<i class="fa-solid fa-puzzle-piece"></i> Best Difficulty ${bestDiff} <i class="fa-solid fa-network-wired"></i><i class="fa-solid fa-puzzle-piece"></i>Difficulty ${networkDifficulty} <i class="fa-solid fa-chart-line"></i> Network/Best ${bestDiffRatio} ×<br>
 			<i class="fa-solid fa-share-nodes"></i> Shares: ${this.dataMiner.shares} <i class="fa-solid fa-xmark"></i> Rejected ${this.dataMiner.rejected} <i class="fa-solid fa-caret-right"></i> ${((this.dataMiner.rejected/this.dataMiner.shares)*100).toFixed(2)} %<br> 
 			<i class="fa-solid fa-network-wired"></i><i class="fa-brands fa-btc"></i> ${Number(BTCoverall).toLocaleString("de-DE")}    <i class="fa-solid fa-file-fragment"></i> ${((BTCoverall/21000000)*100).toFixed(2)}%<br>
 			<i class="fa-solid fa-hourglass-half"></i>Block Interval ${(NextBlockInterval).toFixed(3)} min<br>
 			<i class="fa-solid fa-temperature-high"></i> Temperatur ${this.dataMiner.temp.toFixed(1)} °C <i class="fa-solid fa-bolt"></i> Power ${this.dataMiner.power.toFixed(1)} W<br>
-            <i class="fa-solid fa-water-ladder"></i> Pool ${this.dataMiner.pool}<br>
             <i class="fa-solid fa-power-off"></i> Uptime ${uptimeText} ${sekunden} Sekunden<br>		
 			
 		`;
